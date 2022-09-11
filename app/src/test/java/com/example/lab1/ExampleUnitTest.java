25cfdc1354d8bca2041ed1621d84e20f061cfeea
Domalega <vladimir.domalega@gmail.com>
2022-09-11 21:19:49 +0700

    create JavaDoc

--- app/src/test/java/com/example/lab1/ExampleUnitTest.java
+++ app/src/test/java/com/example/lab1/ExampleUnitTest.java
@@ -6,8 +6,10 @@ import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
- *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ *
+ * @author Vladimir Domalega and Daria Sorokovih
+ *
  */
 public class ExampleUnitTest {
     @Test
